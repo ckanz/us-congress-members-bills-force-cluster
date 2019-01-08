472ff000fc6a6445bdf95313f3059b87222b5864
Clemens Anzmann <clemens.anzmann@googlemail.com>
2019-01-08 22:44:14 +1100

    extract mouse functions

--- src/cluster.js
+++ src/cluster.js
@@ -24,64 +24,68 @@ const getForce = (nodeData, clusterElement) => {
   return myForce;
 };
 
-const renderCircles = (clusterData) => {
+const enterNode = (d, i, e) => {
+  const innerText = select(e[i]).select('.node-text');
+  innerText
+    .transition()
+    .style('opacity', 0)
+    .on('end', () => {
+      innerText
+        .attr('style', d => `height: ${d.radius * 2}px; font-size: ${d.radius / 6}px; line-height:${d.radius / 4}px;`)
+        .text('');
+
+      innerText
+        .append('div')
+        .text(`Party: ${d.raw.party}`)
+        .append('div')
+        .text(`Gender: ${d.raw.gender}`)
+        .append('div')
+        .text(`Votes with party: ${d.raw.detail.roles[0].votes_with_party_pct}%`)
+        .append('div')
+        .text(`Bills (co-)sponsored: ${d.raw.detail.roles[0].bills_sponsored + d.raw.detail.roles[0].bills_cosponsored}`)
+        .append('div')
+        .text(`Role: ${d.raw.role}`)
+        .append('div')
+        .text(`Seniority: ${d.raw.seniority}`)
+        .append('div')
+        .text(`Missed Votes: ${d.raw.detail.roles[0].missed_votes_pct}%`)
+        .transition()
+        .style('opacity', 1);
+    });
+  /*
+  <a href="${d.raw.times_topics_url}" target="_blank">Times Topics |</a>
+  <a href="https://twitter.com/${d.raw.twitter_id}" target="_blank">Twitter Account |</a>
+  <a href="https://facebook.com/${d.raw.facebook_account}" target="_blank">Facebook Account |</a>
+  <a href="https://youtube.com/${d.raw.youtube_id}" target="_blank">Youtube Account |</a>
+  <a href="${d.raw.detail.rss_url}" target="_blank">RSS Feeed |</a>
+  */
+};
+
+const exitNode = (d, i, e) => {
+  const innerText = select(e[i]).select('.node-text');
+    innerText
+      .transition()
+      .style('opacity', 0)
+      .on('end', () => {
+        innerText
+          .attr('style', d => `height: ${d.radius * 2}px; font-size: ${d.radius / 4}px;`)
+          .text(d.text)
+          .transition()
+          .style('opacity', 1);
+      });
+};
+
+const renderCircles = clusterData => {
   const myNodes = select('#cluster')
     .selectAll('g')
     .data(clusterData)
     .enter()
     .append('g')
     .attr('class', 'cluster-node');
-
+  
   myNodes
-    .on('mouseenter', (d, i, e) => {
-      const innerText = select(e[i]).select('.node-text');
-      innerText
-        .transition()
-        .style('opacity', 0)
-        .on('end', () => {
-          innerText
-            .attr('style', d => `height: ${d.radius * 2}px; font-size: ${d.radius / 6}px; line-height:${d.radius / 4}px;`)
-            .text('');
-
-          innerText
-            .append('div')
-            .text(`Party: ${d.raw.party}`)
-            .append('div')
-            .text(`Gender: ${d.raw.gender}`)
-            .append('div')
-            .text(`Votes with party: ${d.raw.detail.roles[0].votes_with_party_pct}%`)
-            .append('div')
-            .text(`Bills (co-)sponsored: ${d.raw.detail.roles[0].bills_sponsored + d.raw.detail.roles[0].bills_cosponsored}`)
-            .append('div')
-            .text(`Role: ${d.raw.role}`)
-            .append('div')
-            .text(`Seniority: ${d.raw.seniority}`)
-            .append('div')
-            .text(`Missed Votes: ${d.raw.detail.roles[0].missed_votes_pct}%`)
-            .transition()
-            .style('opacity', 1);
-        });
-      /*
-      <a href="${d.raw.times_topics_url}" target="_blank">Times Topics |</a>
-      <a href="https://twitter.com/${d.raw.twitter_id}" target="_blank">Twitter Account |</a>
-      <a href="https://facebook.com/${d.raw.facebook_account}" target="_blank">Facebook Account |</a>
-      <a href="https://youtube.com/${d.raw.youtube_id}" target="_blank">Youtube Account |</a>
-      <a href="${d.raw.detail.rss_url}" target="_blank">RSS Feeed |</a>
-      */
-    })
-    .on('mouseleave', (d, i, e) => {
-      const innerText = select(e[i]).select('.node-text');
-      innerText
-        .transition()
-        .style('opacity', 0)
-        .on('end', () => {
-          innerText
-            .attr('style', d => `height: ${d.radius * 2}px; font-size: ${d.radius / 4}px;`)
-            .text(d.text)
-            .transition()
-            .style('opacity', 1);
-        });
-    });
+    .on('mouseenter', enterNode)
+    .on('mouseleave', exitNode);
 
   myNodes
     .append('circle')
