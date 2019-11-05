37edcbacaccd00b1192889bb1277e2216f0bcc65
Clemens Anzmann <clemens.anzmann@gmail.com>
2019-11-05 05:45:10 +0000

    fix test runner

--- karma.conf.js
+++ karma.conf.js
@@ -2,7 +2,7 @@ var webpack = require('webpack');
 
 module.exports = config => {
   config.set({
-    browsers: ['PhantomJS'],
+    browsers: ['ChromeHeadless'],
     singleRun: true,
     frameworks: ['jasmine'],
     files: [
