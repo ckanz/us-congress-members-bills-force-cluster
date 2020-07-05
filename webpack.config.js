662249fdfe757e9f4c9217587fee70b89c4e16dc
Clemens Anzmann <clemens.anzmann@gmail.com>
2020-07-05 20:17:11 +0100

    put scrits in own folder

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/app.js',
+  entry: './src/js/app.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
